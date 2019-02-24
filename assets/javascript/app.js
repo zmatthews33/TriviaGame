6f8f0bc90eef26864d3355c3d65ad2149ec596c9
whatevermane <zmatt33@gmail.com>
2019-02-24 15:22:51 -0600

    Added trivia questions as objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+
+var questions =
+[
+  {
+    question: "Who was the only team to beat Michael Jordan and the Bulls in a playoff series in the 90's?",
+    answers: ["New York Knicks", "Pheonix Suns", "Orlando Magic", "Indiana Pacers" ],
+    correct: "Orlando Magic"
+  },
+
+  {
+    question: "What team won the most championship in the 80's?",
+    answers: ["Los Angeles Lakers", "Boston Celtics", "Detroit Pistons", "Philadelphia 76ers"],
+    correct: "Los Angeles Lakers"
+  },
+  {
+    question: "Who won the dunk contest in 1988 with a dunk from the freethrow line?",
+    answers: ["Julius Erving", "Larry Bird", "Vince Carter", "Dominique Wilkins"],
+    correct: "Michael Jordan"
+  },
+  {
+    question: "Other than Oscar Robertson, who is the only player to lead his team in points per game, rebounds per game, assists per game, steals per game, free throw percentage for starters, and 3-pt percentage for starters, for at least 94% of his career ?",
+    answers: ["Michael Jordan", "Wilt Chamberlain", "LeBron James", "Larry Bird"],
+    correct: "Larry Bird"
+  },
+  {
+    question: "What NBA Power Forward played an alter-ego character by the name of 'Grandmama' on the hit TV show 'Family Matters' and in Converse ads?",
+    answers: ["Larry Johnson", "Kevin McHale", "Patrick Ewing", "Horace Grant"],
+    correct: "Larry Johnson"
+  },
+  {
+    question: "What player has a record 7 rebounding titles?",
+    answers: ["Larry Bird", "Shaquille O'Neal", "Bill Laimbeer", "Dennis Rodman"],
+    correct: "Dennis Rodman"
+  },
+  {
+    question: "What team has the most NBA championships, winning 3 in the 80's?",
+    answers: ["Chicago Bulls", "Los Angeles Lakers", "Boston Celtics", "None of the Above"],
+    correct: "Boston Celtics"
+  },
+  {
+    question: "Who is known as the most dominant Center of the 90's?",
+    answers: ["Shaquille O'Neal", "Patrick Ewing", "Hakeem Olajuwon", "Luc Longley"],
+    correct: "Shaquille O'Neal"
+  },
+  {
+    question: "What team won 6 championships in the 90's?",
+    answers: ["Detroit Pistons", "Houston Rockets", "Utah Jazz", "Chicago Bulls"],
+    correct: "Chicago Bulls"
+  },
+  {
+    question: "Who won rookie of the year in the '92-'93 season?",
+    answers: ["Anfernee Hardaway", "Shaquille O'Neal","Chris Webber", "Kobe Bryant"],
+    correct: "Shaquille O'Nealhaq"
+  }
